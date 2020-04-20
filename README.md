6ac38c04ba2131e9069510e868bb9658b1214a83
etinaude <etinaude@gmail.com>
2020-04-20 20:29:27 +1200

    added quick sort

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ please feel free to add and edit
 
 * bubble sort
 * counting sort
+* quick sort
 
 ## These will be added:
 
 * pancake sort
 * radix sort
 * merge sort
-* quick sort
 * insertion sort
 * bogo sort
 * pigeonhole sort
--- sort.py
+++ sort.py
@@ -6,10 +6,10 @@ import pickle
 import math
 
 array = []
-size = 100
+size = 10
 count =[0]
 out =[0]
-rng = 100
+rng = 10
 
 #intialize
 def srt():
@@ -28,8 +28,11 @@ def store(data):
 #bubble sort
 def bubble():
     global size, array, rng, out, count
+    #big loop
     for i in range(0, size-1):
+        #small loop
         for j in range(0, size-1):
+            #check and perform swap
             if array[j] > array[j+1]:
              temp = array[j]
              array[j] = array[j+1]
@@ -40,19 +43,46 @@ def bubble():
 def counting():
     global size, array, rng, out, count
     total = [0]
+    #count frequency
     for i in range(size):
         count[array[i]] += 1
     total[0]=count[0]
-
+    #add frequency
     for i in range(1,rng+1):
         total.append(count[i]+total[i-1])
     total[0]=0
-
+    #insert into final array
     for i in array:
         out[total[i-1]] = i
         total[i-1] += 1
     print(out[:-1])
     
+#quick sort
+def quick(arrays,within = False):
+    low = []
+    high = []
+    # end recurssion
+    if len(arrays) <= 1:
+        return arrays
+    else:
+        #set pivot
+        pivot = arrays[0]
+        #seperate items
+        for i in arrays[1:]:
+            if i<pivot:
+                low.append(i)
+            else:
+                high.append(i)
+    #quick sort low items
+    low = quick(low, True)
+    #quick sort high items
+    high = quick(high,True)
+    arrays = low + [pivot]+high
+    #if its fully sorted print the array
+    if within == False:
+            print(arrays)
+    return arrays
+
 #radix sort
 def radix():
     pass
@@ -63,10 +93,6 @@ def merge_sort():
     pass
     #comming soon
 
-#quick sort
-def quick():
-    pass
-    #comming soon
 
 #insertion sort
 def insertion():
@@ -102,4 +128,5 @@ def pancake():
 srt()
 print(array)
 print()
-bubble()
+quick(array)
+print(sorted(array))
